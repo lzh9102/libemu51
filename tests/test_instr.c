61feeced364abbdd6109ae30818f39828c7497e2
Che-Huai Lin <lzh9102@gmail.com>
2015-04-18 21:43:40 +0800

    Add test for instruction NOP

--- tests/test_instr.c
+++ tests/test_instr.c
@@ -19,6 +19,9 @@
 #define calloc test_calloc
 #define free test_free
 
+#define PMEM_SIZE 4096         /* program memory size used in tests */
+#define XRAM_SIZE (32 * 1024)  /* external memory size used in tests */
+
 /* data used in unit tests */
 typedef struct testdata
 {
@@ -54,11 +57,11 @@ testdata *alloc_test_data()
 	data->iram_upper = calloc(128, 1);
 
 	/* 4KB of program memory */
-	data->pmem_len = 4 * 1024;
+	data->pmem_len = PMEM_SIZE;
 	data->pmem = calloc(data->pmem_len, 1);
 
-	/* 32KB of external RAM */
-	data->xram_len = 32 * 1024;
+	/* external RAM */
+	data->xram_len = XRAM_SIZE;
 	data->xram = calloc(data->xram_len, 1);
 
 	/* setup the emulator */
@@ -87,16 +90,58 @@ void free_test_data(testdata *data)
 	free(data);
 }
 
+/* write random data to the buffer */
+void write_random_data(uint8_t *buffer, size_t size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		buffer[i] = rand() & 0xff;
+}
+
 /* here goes the test functions */
 
-/* TODO: add tests here */
+void test_nop(void **state)
+{
+	testdata *data = alloc_test_data();
+	emu51 *m = data->m;
+
+	/* fill in NOP in pmem[0] */
+	data->pmem[0] = 0x00;
+
+	/* fill in random data to emulator memory */
+	write_random_data(m->sfr, 128);
+	write_random_data(m->iram_lower, 128);
+	write_random_data(m->iram_upper, 128);
+	write_random_data(m->xram, XRAM_SIZE);
+
+	/* save the original memory and SFRs */
+	uint8_t sfr[128], iram_lower[128], iram_upper[128];
+	uint8_t *xram = malloc(XRAM_SIZE); /* xram is big, don't put it on stack */
+	memcpy(sfr, m->sfr, 128);
+	memcpy(iram_lower, m->iram_lower, 128);
+	memcpy(iram_upper, m->iram_upper, 128);
+	memcpy(xram, m->xram, XRAM_SIZE);
+
+	/* execute nop */
+	const emu51_instr *instr = emu51_get_instr(0x00); /* NOP */
+	instr->handler(instr, &m->pmem[0], m);
+
+	/* nop shouldn't change any of the SFRs, iram or xram */
+	assert_memory_equal(sfr, m->sfr, 128);
+	assert_memory_equal(iram_lower, m->iram_lower, 128);
+	assert_memory_equal(iram_upper, m->iram_upper, 128);
+	assert_memory_equal(xram, m->xram, XRAM_SIZE);
+
+	free(xram);
+	free_test_data(data);
+}
 
 /* end of test functions */
 
 int main()
 {
 	const struct CMUnitTest tests[] = {
-		/* TODO: add tests here */
+		cmocka_unit_test(test_nop),
 	};
 	/* don't use setup and teardown as cmocka doesn't report memory bugs in them
 	 */
