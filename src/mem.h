65a4d1c595a966b751ca374423f7aea9501feb87
Che-Huai Lin <lzh9102@gmail.com>
2015-05-01 22:44:53 +0800

    Extract memory-accessing functions to mem.h

---
+++ src/mem.h
@@ -0,0 +1,92 @@
+/* memory accessing functions */
+
+#ifndef _MEM_H_
+#define _MEM_H_
+
+#include <emu51.h>
+
+/* Read data from immediate address.
+ * An immediate address can refer to:
+ *  1. internal ram, if addr < 0x80
+ *  2. SFR, if addr >= 0x80
+ */
+static inline uint8_t direct_addr_read(emu51 *m, uint8_t addr)
+{
+	if (addr < 0x80) /* lower internal ram (0~0x7f) */
+		return m->iram_lower[addr];
+	else /* SFR */
+		return m->sfr[addr - SFR_BASE_ADDR];
+}
+
+/* Write data to direct address */
+static inline void direct_addr_write(emu51 *m, uint8_t addr, uint8_t data)
+{
+	if (addr < 0x80) /* lower internal ram (0~0x7f) */
+		m->iram_lower[addr] = data;
+	else /* SFR */
+		m->sfr[addr - SFR_BASE_ADDR] = data;
+}
+
+/* Read data from the address obtained by dereferencing ptr.
+ *
+ * ptr: address of an iram byte (ptr < 0x80) or a SFR (ptr >= 0x80).
+ *
+ * Example: If ptr == 0xe0 (address of ACC) and the content of ACC is 0x30,
+ * the content of address 0x30 will be written to *out.
+ *
+ * The indirect address always refer to internal ram, not SFR.
+ * Returns 0 on success or return an error code. The caller must check for the
+ * error code.
+ */
+static inline int indirect_addr_read(emu51 *m, uint8_t ptr, uint8_t *out)
+{
+	uint8_t addr = direct_addr_read(m, ptr);
+	if (addr < 0x80) { /* lower iram */
+		/* m->iram_lower is required to be set by the user, so we don't have to
+		 * check for NULL here. */
+		*out = m->iram_lower[addr];
+		return 0;
+	} else { /* upper iram */
+		/* m->iram_upper is not guaranteed to exist, so checking is necessary */
+		if (m->iram_upper) {
+			*out = m->iram_upper[addr - 0x80];
+			return 0;
+		} else { /* upper iram is not set */
+			return EMU51_IRAM_OUT_OF_RANGE;
+		}
+	}
+}
+
+/* Write data to the address obtained by dereferencing ptr.
+ * See indirect_addr_read for details.
+ */
+static inline int indirect_addr_write(emu51 *m, uint8_t ptr, uint8_t data)
+{
+	uint8_t addr = direct_addr_read(m, ptr);
+	if (addr < 0x80) { /* lower iram */
+		/* m->iram_lower is required to be set by the user, so we don't have to
+		 * check for NULL here. */
+		m->iram_lower[addr] = data;
+		return 0;
+	} else { /* upper iram */
+		/* m->iram_upper is not guaranteed to exist, so checking is necessary */
+		if (m->iram_upper) {
+			m->iram_upper[addr - 0x80] = data;
+			return 0;
+		} else { /* upper iram is not set */
+			return EMU51_IRAM_OUT_OF_RANGE;
+		}
+	}
+}
+
+/* Push a value onto the stack. The SP is first incremented, and
+ * the data is then written to the position pointed by the new SP.
+ * Returns 0 on success or EMU51_IRAM_OUT_OF_RANGE on stack overflow.
+ */
+static inline int stack_push(emu51 *m, uint8_t data)
+{
+	++m->sfr[SFR_SP];
+	return indirect_addr_write(m, SFR_BASE_ADDR + SFR_SP, data);
+}
+
+#endif /* _MEM_H_ */
